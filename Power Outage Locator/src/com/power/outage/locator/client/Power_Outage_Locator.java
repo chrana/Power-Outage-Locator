dbff935de3589a1256462de61ec4da6b711fe7f1
Chirag <Chirag@RanaChirag-PC>
2014-01-20 13:26:30 -0500

    Created an empty Project

---
+++ Power Outage Locator/src/com/power/outage/locator/client/Power_Outage_Locator.java
@@ -0,0 +1,13 @@
+package com.power.outage.locator.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Power_Outage_Locator implements EntryPoint {
+
+	public void onModuleLoad() {
+		
+	}
+}
